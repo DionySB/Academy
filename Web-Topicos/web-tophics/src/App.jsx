d17772b0178fd686fb053305b27f0a4bd3d84843
diony <contato.diony@gmail.com>
2023-03-21 17:05:36 -0300

    fix(Web-Topicos): fix web-tophics

--- Web-Topicos/web-tophics/src/App.jsx
+++ Web-Topicos/web-tophics/src/App.jsx
@@ -3,5 +3,7 @@ import { Prompt } from "./components/Prompt"
 import './App.css'
 
 export function App(props) {
-  return <h1>Olá, {props.name}</h1>;
+  return (
+    <Prompt />
+  )
 }
--- Web-Topicos/web-tophics/src/components/Prompt.jsx
+++ Web-Topicos/web-tophics/src/components/Prompt.jsx
@@ -1,10 +1,7 @@
-export function Prompt() {
-    return (
-      <section className='prompt'>
-        <img src={qrCode} alt="Atividade 20/03" />
-        <br></br>
-        <strong>Fixed the activity from Tópicos avançados em Ciência da Computação</strong>
-        <p>Click Here!</p>
-      </section>
-    )
+export function Prompt(props) {
+  return (
+    <section className='css1'>
+    <strong>Olá, {props.name}</strong>
+    </section>
+  )
 }
